ad96c065bc2b0a8a292ea507be3b99018b1f1d8f
NorgeMan <ivan.veremeichik@gmail.com>
2021-02-22 08:14:00 +0300

    "1 commit"

---
+++ src/actions/userList.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+import "bootstrap/dist/css/bootstrap.css";
+import {setUsers} from "../reducers/userListReducer";
+
+export function getUsers() {
+    return async dispatch => {
+        try {
+            alert('Token: ' + localStorage.getItem('token'))
+            const response = await axios.get(`http://localhost:5000/api/users`,
+                       {headers: {Authorization: `Bearer ${localStorage.getItem('token')}`}}
+            )
+            dispatch(setUsers(response.data))
+            console.log(response.data)
+        } catch (e) {
+            alert(e.message)
+        }
+    }
+}
---
+++ src/components/App.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import Navbar from "./navbar/Navbar";
+import './app.css';
+import {BrowserRouter, Route, Switch,Redirect} from "react-router-dom";
+import Registration from './authorization/Registration';
+import Login from "./authorization/Login";
+import {useDispatch, useSelector} from "react-redux";
+import {auth} from "../actions/user"
+import 'bootstrap/dist/css/bootstrap.css';
+import {useEffect} from "react/cjs/react.production.min";
+import HomePage from "./homepage/HomePage.jsx";
+
+function App() {
+    const isAuth = useSelector(state => state.user.isAuth)
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        dispatch(auth())
+    }, [])
+
+
+    return (
+        <BrowserRouter>
+            <div className='app'>
+                <Navbar/>
+                <div className='wrap'>
+                    {!isAuth ?
+                    <Switch>
+                        <Route path="/registration" component={Registration}/>
+                        <Route path="/login" component={Login}/>
+                        <Redirect to="/login"/>
+                    </Switch>
+                    :
+                    <Switch>
+                        <Route exact path="/" component={HomePage}/>
+                        <Redirect to= "/" />
+                    </Switch>
+                    }
+                </div>
+            </div>
+        </BrowserRouter>
+    );
+}
+
+export default App;
